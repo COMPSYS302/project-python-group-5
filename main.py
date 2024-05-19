fae1fa23fce3b75b392dd88f6129cadedc7cc7ad
R4H3N <rdhi934@aucklanduni.ac.nz>
2024-05-19 18:48:08 +1200

    Changes made to app

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+from PyQt5.QtCore import QCoreApplication
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QFont
 
 def main():
     app = QApplication([])
     window = QWidget()
+    window.setGeometry(500,500,500,500)
+    window.setWindowTitle("My App")
+
+    b1 = QPushButton(window)
+    b1.setText("Click Me")
+    b1.clicked.connect(QCoreApplication.instance().quit)
+
     window.show()
     app.exec_()
 
