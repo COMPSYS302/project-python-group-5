3228aa4b2e7e670fb12f025b48ae9e9bb137e5f4
DevenTri <dtri559@aucklanduni.ac.nz>
2024-06-05 23:32:29 +1200

    fixed all models and camera window

--- models.py
+++ models.py
@@ -45,17 +45,16 @@ class CustomCNN(nn.Module):
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
         self.conv3 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(256 * 4 * 4, 512)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(256 * 28 * 28, 512)
         self.fc2 = nn.Linear(512, num_classes)
 
     def forward(self, x):
-        x = torch.relu(self.conv1(x))
-        x = torch.max_pool2d(x, 2)
-        x = torch.relu(self.conv2(x))
-        x = torch.max_pool2d(x, 2)
-        x = torch.relu(self.conv3(x))
-        x = torch.max_pool2d(x, 2)
+        x = self.pool(torch.relu(self.conv1(x)))
+        x = self.pool(torch.relu(self.conv2(x)))
+        x = self.pool(torch.relu(self.conv3(x)))
         x = torch.flatten(x, 1)
         x = torch.relu(self.fc1(x))
         x = self.fc2(x)
         return x
+
